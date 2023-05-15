9c3fe0612100102fadd217ddfdef61754d5f9bc3
kyoungmin <stephen4951@gmail.com>
2023-05-15 12:26:33 +0900

    230515 mybatis로 score 처리

--- SpringDBAccess/src/main/java/com/spring/db/service/BoardService.java
+++ SpringDBAccess/src/main/java/com/spring/db/service/BoardService.java
@@ -3,6 +3,7 @@ package com.spring.db.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 import com.spring.db.model.BoardVO;
@@ -12,6 +13,7 @@ import com.spring.db.repository.IBoardDAO;
 public class BoardService implements IBoardService {
 
 	@Autowired
+	@Qualifier("boardDAO")
 	private IBoardDAO dao;
 	
 	@Override
