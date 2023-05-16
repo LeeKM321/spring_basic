e47144da60261db54aec307deac495d06fcde2f5
kyoungmin <stephen4951@gmail.com>
2023-05-16 11:26:32 +0900

    230516 freeboardMapper 테스트 완료

--- MyWeb/src/test/java/com/spring/myweb/freeboard/mapper/FreeBoardMapperTest.java
+++ MyWeb/src/test/java/com/spring/myweb/freeboard/mapper/FreeBoardMapperTest.java
@@ -1,6 +1,8 @@
 package com.spring.myweb.freeboard.mapper;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.List;
 
@@ -34,9 +36,9 @@ public class FreeBoardMapperTest {
 		
 		//given: 테스트를 위해 주어질 데이터 (ex: parameter)
 		FreeBoardVO vo = new FreeBoardVO();
-//		vo.setTitle("첫번째 글");
+		vo.setTitle("두번째 글");
 		vo.setWriter("abc1234");
-		vo.setContent("안녕하세요~ 반갑습니다!");
+		vo.setContent("메롱메롱");
 		
 		//when: 테스트 실제 상황
 		mapper.regist(vo);
@@ -59,7 +61,60 @@ public class FreeBoardMapperTest {
 		
 		list.forEach(vo -> System.out.println(vo));
 		
-		assertEquals(1, list.size());
+		assertEquals(2, list.size());
+		
+	}
+	
+	@Test
+	@DisplayName("글 번호가 2번인 글을 조회하면 "
+			+ "글쓴이는 'abc1234'일 것이고 글 내용은 '메롱메롱' 일 것이다.")
+	void getContentTest() {
+		
+		//given
+		int bno = 2;
+		
+		//when
+		FreeBoardVO vo = mapper.getContent(bno);
+		
+		//then
+		assertEquals("abc1234", vo.getWriter());
+		assertEquals("메롱메롱", vo.getContent());
+//		assertTrue(vo.getContent().equals("메롱메롱"));
+//		assertNull(vo);
+		
+	}
+	
+	@Test
+	@DisplayName("글 번호가 1번인 글의 제목과 내용을 수정 후 다시 조회했을 때 "
+			+ "제목이 수정한 제목으로 바뀌었음을 단언한다.")
+	void updateTest() {
+		//given
+		FreeBoardVO vo = new FreeBoardVO();
+		vo.setBno(1);
+		vo.setTitle("수정된 글 제목");
+		vo.setContent("수정된 글 내용");
+		
+		//when
+		mapper.update(vo);
+		
+		//then
+		assertEquals(vo.getTitle(), mapper.getContent(vo.getBno()).getTitle());
+	}
+	
+	@Test
+	@DisplayName("글 번호가 2번인 글을 삭제한 후에 리스트를 전체 조회했을 때 "
+			+ "글의 개수는 1개일 것이고, "
+			+ "2번 글을 조회했을 때 null이 반환되어야 한다.")
+	void deleteTest() {
+		//given
+		int bno = 2;
+		
+		//when
+		mapper.delete(bno);
+		
+		//then
+		assertEquals(1, mapper.getList().size());
+		assertNull(mapper.getContent(bno));
 		
 	}
 	
