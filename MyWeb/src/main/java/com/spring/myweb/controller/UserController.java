ef9c3af37c2188850efaca1a5bec015743479e95
kyoungmin <stephen4951@gmail.com>
2023-05-22 12:28:28 +0900

    230522 userMypage 페이징 처리

--- MyWeb/src/main/java/com/spring/myweb/controller/UserController.java
+++ MyWeb/src/main/java/com/spring/myweb/controller/UserController.java
@@ -13,8 +13,10 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.spring.myweb.command.UserVO;
+import com.spring.myweb.freeboard.service.IFreeBoardService;
 import com.spring.myweb.user.service.IUserService;
 import com.spring.myweb.util.MailSenderService;
+import com.spring.myweb.util.PageCreator;
 import com.spring.myweb.util.PageVO;
 
 import lombok.extern.slf4j.Slf4j;
@@ -26,6 +28,8 @@ public class UserController {
 
 	@Autowired
 	private IUserService service;
+	@Autowired
+	private IFreeBoardService boardService;
 	@Autowired
 	private MailSenderService mailService;
 	
@@ -78,8 +82,9 @@ public class UserController {
 		
 		//세션 데이터에서 id를 뽑아야 sql을 돌릴 수 있겠죠?
 		String id = (String) session.getAttribute("login");
-		UserVO user = service.getInfo(id, vo);
-		
+		PageCreator pc = new PageCreator(vo, boardService.getTotal(vo));
+		model.addAttribute("userInfo", service.getInfo(id, vo));
+		model.addAttribute("pc", pc);
 	}
 	
 	
